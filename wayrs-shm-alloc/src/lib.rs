b83e0f2c4c9aa5f50c2d4a210e65ac5b99d7f56a
MaxVerevkin <maxxverrr@gmail.com>
2023-02-09 17:34:21 +0200

    wayrs_shm_alloc: make the API more flexible and harder to misuse

--- wayrs-shm-alloc/src/lib.rs
+++ wayrs-shm-alloc/src/lib.rs
@@ -2,6 +2,8 @@
 
 use std::fs::File;
 use std::os::unix::io::{BorrowedFd, FromRawFd};
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::Arc;
 
 use memmap2::MmapMut;
 
@@ -11,88 +13,58 @@ use wayrs_client::protocol::*;
 use wl_shm::{Format, WlShm};
 use wl_shm_pool::WlShmPool;
 
-macro_rules! alloc_id {
-    ($self:ident) => {{
-        let id = $self.next_id;
-        $self.next_id += 1;
-        id
-    }};
-}
-
 /// A simple "free list" shared memory allocator
 #[derive(Debug)]
 pub struct ShmAlloc {
+    wl_shm: WlShm,
+    pool: Option<InitShmPoll>,
+}
+
+#[derive(Debug)]
+struct InitShmPoll {
     pool: WlShmPool,
     len: usize,
     file: File,
     mmap: MmapMut,
     segments: Vec<Segment>,
-    next_id: u64,
-}
-
-/// Implement this trait on your Wayland state struct to use [`ShmAlloc`]
-pub trait ShmAllocState: Sized + 'static {
-    /// Get a mutable reference to [`ShmAlloc`].
-    ///
-    /// This implies that only a single [`ShmAlloc`] can be used.
-    fn shm_alloc(&mut self) -> &mut ShmAlloc;
 }
 
 #[derive(Debug)]
 struct Segment {
-    id: u64,
     offset: usize,
     len: usize,
-    refcnt: u32,
-    buffer: Option<Buffer>,
+    refcnt: Arc<AtomicU32>,
+    buffer: Option<(WlBuffer, BufferSpec)>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct BufferSpec {
+    pub width: u32,
+    pub height: u32,
+    pub stride: u32,
+    pub format: Format,
+}
+
+impl BufferSpec {
+    pub fn size(&self) -> usize {
+        self.stride as usize * self.height as usize
+    }
 }
 
 /// A `wl_buffer` with some metadata.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub struct Buffer {
-    pub id: u64,
-    pub wl: WlBuffer,
-    pub width: i32,
-    pub height: i32,
-    pub stride: i32,
-    pub format: Format,
+    spec: BufferSpec,
+    wl: WlBuffer,
+    refcnt: Arc<AtomicU32>,
+    wl_shm_pool: WlShmPool,
+    offset: usize,
 }
 
 impl ShmAlloc {
     /// Create new [`ShmAlloc`].
-    ///
-    /// Only one instance of [`ShmAlloc`] can be practically used (see [`ShmAllocState::shm_alloc`]).
-    /// This limitation might get resolved in the future.
-    pub fn new<D: ShmAllocState>(
-        conn: &mut Connection<D>,
-        wl_shm: WlShm,
-        initial_len: usize,
-    ) -> Self {
-        let fd = shmemfdrs::create_shmem(wayrs_client::cstr!("/wayrs_shm_pool"), initial_len);
-        let file = unsafe { File::from_raw_fd(fd) };
-        let mmap = unsafe { MmapMut::map_mut(&file).expect("memory mapping failed") };
-
-        let fd_dup = unsafe {
-            BorrowedFd::borrow_raw(fd)
-                .try_clone_to_owned()
-                .expect("could not duplicate fd")
-        };
-        let pool = wl_shm.create_pool(conn, fd_dup, initial_len as i32);
-
-        Self {
-            pool,
-            len: initial_len,
-            file,
-            mmap,
-            segments: vec![Segment {
-                id: 0,
-                offset: 0,
-                len: initial_len,
-                refcnt: 0,
-                buffer: None,
-            }],
-            next_id: 1,
-        }
+    pub fn new(wl_shm: WlShm) -> Self {
+        Self { wl_shm, pool: None }
     }
 
     /// Allocate a new buffer.
@@ -101,118 +73,154 @@ impl ShmAlloc {
     /// reused whenever possible.
     ///
     /// See [`WlShmPool::create_buffer`] for more info.
-    pub fn alloc_buffer<D: ShmAllocState>(
+    pub fn alloc_buffer<D>(
         &mut self,
         conn: &mut Connection<D>,
-        width: i32,
-        height: i32,
-        stride: i32,
-        format: Format,
+        spec: BufferSpec,
     ) -> (Buffer, &mut [u8]) {
-        let size = height * stride;
+        self.pool
+            .get_or_insert_with(|| InitShmPoll::new(conn, self.wl_shm, spec.size()))
+            .alloc_buffer(conn, spec)
+    }
+}
 
-        let segment_index = self.alloc_segment(conn, size as usize, format);
-        let segment = &mut self.segments[segment_index];
-
-        let buffer = *segment.buffer.get_or_insert_with(|| {
-            let wl = self.pool.create_buffer_with_cb(
-                conn,
-                segment.offset as i32,
-                width,
-                height,
-                stride,
-                format,
-                |_conn, state, wl_buffer, event| {
-                    let wl_buffer::Event::Release = event;
-                    state.shm_alloc().free_buffer(wl_buffer);
-                    // We don't destroy the buffer here because it can be reused later
-                },
-            );
-            Buffer {
-                id: segment.id,
-                wl,
-                width,
-                height,
-                stride,
-                format,
-            }
-        });
-
-        let bytes = &mut self.mmap[segment.offset..][..segment.len];
-
-        (buffer, bytes)
+impl Buffer {
+    /// Get the underlying `wl_buffer`.
+    ///
+    /// This `wl_buffer` must be attached to exactly one surface, otherwise the memory may be
+    /// leaked or a panic may occur during [`Connection::dispatch_events`].
+    #[must_use = "memory is leaked if wl_buffer is not attached"]
+    pub fn into_wl_buffer(self) -> WlBuffer {
+        let wl = self.wl;
+        std::mem::forget(self);
+        wl
     }
 
-    /// Create a buffer that shares the underlying memory with another buffer.
+    /// Create a `wl_buffer` that shares the same spec and underlying memory as `self`.
     ///
-    /// `buffer_id` must be the value of [`Buffer::id`] of a buffer that needs to be duplicated.
+    /// This `wl_buffer` must be attached to exactly one surface, otherwise the memory may be
+    /// leaked or a panic may occur during [`Connection::dispatch_events`] or `self`'s drop.
     ///
-    /// Returns `None` if a buffer with `buffer_id` could not be found, either because the id is
-    /// wrong or because the buffer has been freed and reused.
-    ///
-    /// Calling this function for an attached and then released buffer may succeed, but it is not
-    /// guaranteed. For reliable results duplicate only non-attached buffers.
-    pub fn duplicate_buffer<D: ShmAllocState>(
-        &mut self,
-        conn: &mut Connection<D>,
-        buffer_id: u64,
-    ) -> Option<Buffer> {
-        let segment = self.segments.iter_mut().find(|s| s.id == buffer_id)?;
-        let buffer = segment.buffer?;
-        segment.refcnt += 1;
-        let wl = self.pool.create_buffer_with_cb(
+    /// This method is usefull if you want to attach the same buffer to a number of surfaces. In
+    /// fact, this is the only correct way to do it unisg this library.
+    #[must_use = "memory is leaked if wl_buffer is not attached"]
+    pub fn duplicate<D>(&self, conn: &mut Connection<D>) -> WlBuffer {
+        self.refcnt.fetch_add(1, Ordering::AcqRel);
+        let refcnt = Arc::clone(&self.refcnt);
+        self.wl_shm_pool.create_buffer_with_cb(
             conn,
-            segment.offset as i32,
-            buffer.width,
-            buffer.height,
-            buffer.stride,
-            buffer.format,
-            move |conn, state, wl_buffer, event| {
+            self.offset as i32,
+            self.spec.width as i32,
+            self.spec.height as i32,
+            self.spec.stride as i32,
+            self.spec.format,
+            move |conn, _state, wl_buffer, event| {
                 let wl_buffer::Event::Release = event;
-                state
-                    .shm_alloc()
-                    .segments
-                    .iter_mut()
-                    .find(|s| s.id == buffer_id)
-                    .expect("segment for a released buffer not found")
-                    .refcnt -= 1;
+                assert!(refcnt.fetch_sub(1, Ordering::AcqRel) > 0);
                 wl_buffer.destroy(conn);
             },
-        );
-        Some(Buffer { wl, ..buffer })
+        )
     }
 
-    /// Call this function only if you created a buffer, did non attach it to any surface and
-    /// decided that you will not use it. **Attached buffers are freed automatically.**
-    pub fn free_buffer(&mut self, buffer: WlBuffer) {
-        for segment in &mut self.segments {
-            if let Some(b) = &segment.buffer {
-                if b.wl == buffer {
-                    assert!(segment.refcnt > 0);
-                    segment.refcnt -= 1;
-                    break;
-                }
-            }
+    /// Get the spec of this buffer
+    pub fn spec(&self) -> BufferSpec {
+        self.spec
+    }
+}
+
+impl Drop for Buffer {
+    fn drop(&mut self) {
+        assert!(self.refcnt.fetch_sub(1, Ordering::AcqRel) > 0);
+    }
+}
+
+impl InitShmPoll {
+    fn new<D>(conn: &mut Connection<D>, wl_shm: WlShm, size: usize) -> InitShmPoll {
+        let fd = shmemfdrs::create_shmem(wayrs_client::cstr!("/wayrs_shm_pool"), size);
+        let file = unsafe { File::from_raw_fd(fd) };
+        let mmap = unsafe { MmapMut::map_mut(&file).expect("memory mapping failed") };
+
+        let fd_dup = unsafe {
+            BorrowedFd::borrow_raw(fd)
+                .try_clone_to_owned()
+                .expect("could not duplicate fd")
+        };
+        let pool = wl_shm.create_pool(conn, fd_dup, size as i32);
+
+        Self {
+            pool,
+            len: size,
+            file,
+            mmap,
+            segments: vec![Segment {
+                offset: 0,
+                len: size,
+                refcnt: Arc::new(AtomicU32::new(0)),
+                buffer: None,
+            }],
         }
     }
 
-    fn merge_segments<D>(&mut self, conn: &mut Connection<D>) {
+    fn alloc_buffer<D>(
+        &mut self,
+        conn: &mut Connection<D>,
+        spec: BufferSpec,
+    ) -> (Buffer, &mut [u8]) {
+        let size = spec.height * spec.stride;
+
+        let segment_index = self.alloc_segment(conn, size as usize, spec);
+        let segment = &mut self.segments[segment_index];
+
+        let (wl, spec) = *segment.buffer.get_or_insert_with(|| {
+            let seg_refcnt = Arc::clone(&segment.refcnt);
+            let wl = self.pool.create_buffer_with_cb(
+                conn,
+                segment.offset as i32,
+                spec.width as i32,
+                spec.height as i32,
+                spec.stride as i32,
+                spec.format,
+                move |_conn, _state, _wl_buffer, event| {
+                    let wl_buffer::Event::Release = event;
+                    assert!(seg_refcnt.fetch_sub(1, Ordering::SeqCst) > 0);
+                    // We don't destroy the buffer here because it can be reused later
+                },
+            );
+            (wl, spec)
+        });
+
+        (
+            Buffer {
+                spec,
+                wl,
+                refcnt: Arc::clone(&segment.refcnt),
+                wl_shm_pool: self.pool,
+                offset: segment.offset,
+            },
+            &mut self.mmap[segment.offset..][..segment.len],
+        )
+    }
+
+    fn defragment<D>(&mut self, conn: &mut Connection<D>) {
         let mut i = 0;
         while i + 1 < self.segments.len() {
-            if self.segments[i].refcnt != 0 || self.segments[i + 1].refcnt != 0 {
+            // `refcnt`s are only incremented from Self's methods. Since we have `&mut self`,
+            // `refcnt`s can only decrease during the execution of this function.
+            if self.segments[i].refcnt.load(Ordering::SeqCst) != 0
+                || self.segments[i + 1].refcnt.load(Ordering::SeqCst) != 0
+            {
                 i += 1;
                 continue;
             }
 
             if let Some(buffer) = self.segments[i].buffer.take() {
-                buffer.wl.destroy(conn);
+                buffer.0.destroy(conn);
             }
             if let Some(buffer) = self.segments[i + 1].buffer.take() {
-                buffer.wl.destroy(conn);
+                buffer.0.destroy(conn);
             }
 
             self.segments[i].len += self.segments[i + 1].len;
-            self.segments[i].id = alloc_id!(self);
 
             self.segments.remove(i + 1);
         }
@@ -228,84 +236,101 @@ impl ShmAlloc {
     }
 
     // Returns segment index, does not resize
-    fn try_alloc_in_place<D>(&mut self, conn: &mut Connection<D>, len: usize) -> Option<usize> {
+    fn try_alloc_in_place<D>(
+        &mut self,
+        conn: &mut Connection<D>,
+        len: usize,
+        spec: BufferSpec,
+    ) -> Option<usize> {
+        fn take_if_free(s: &Segment) -> bool {
+            s.refcnt
+                .compare_exchange(0, 1, Ordering::AcqRel, Ordering::Acquire)
+                .is_ok()
+        }
+
         // Find a segment with exact size
-        for (i, segment) in self.segments.iter_mut().enumerate() {
-            if segment.refcnt == 0 && segment.len == len {
-                if let Some(buffer) = segment.buffer.take() {
-                    buffer.wl.destroy(conn);
+        if let Some((i, segment)) = self
+            .segments
+            .iter_mut()
+            .enumerate()
+            .filter(|(_, s)| s.len == len)
+            .find(|(_, s)| take_if_free(s))
+        {
+            if let Some(buffer) = &segment.buffer {
+                if buffer.1 != spec {
+                    buffer.0.destroy(conn);
+                    segment.buffer = None;
                 }
-                segment.refcnt = 1;
-                return Some(i);
             }
+            return Some(i);
         }
+
         // Find a segment large enough
-        for (i, segment) in self.segments.iter_mut().enumerate() {
-            if segment.refcnt == 0 && segment.len > len {
-                let offset = segment.offset;
-                if let Some(buffer) = segment.buffer.take() {
-                    buffer.wl.destroy(conn);
-                }
-                segment.offset += len;
-                segment.len -= len;
-                segment.id = alloc_id!(self);
-                self.segments.insert(
-                    i,
-                    Segment {
-                        id: alloc_id!(self),
-                        offset,
-                        len,
-                        refcnt: 1,
-                        buffer: None,
-                    },
-                );
-                return Some(i);
+        if let Some((i, segment)) = self
+            .segments
+            .iter_mut()
+            .enumerate()
+            .filter(|(_, s)| s.len > len)
+            .find(|(_, s)| take_if_free(s))
+        {
+            if let Some(buffer) = segment.buffer.take() {
+                buffer.0.destroy(conn);
             }
+            let extra = segment.len - len;
+            let offset = segment.offset + len;
+            segment.len = len;
+            self.segments.insert(
+                i + 1,
+                Segment {
+                    offset,
+                    len: extra,
+                    refcnt: Arc::new(AtomicU32::new(0)),
+                    buffer: None,
+                },
+            );
+            return Some(i);
         }
+
         None
     }
 
     // Returns segment index
-    fn alloc_segment<D>(&mut self, conn: &mut Connection<D>, len: usize, format: Format) -> usize {
-        // Find a segment with exact size and a matching buffer
-        for (i, segment) in self.segments.iter_mut().enumerate() {
-            if segment.refcnt == 0 && segment.len == len {
-                if let Some(b) = segment.buffer {
-                    if b.format == format {
-                        segment.refcnt = 1;
-                        return i;
-                    }
-                }
-            }
-        }
-
-        if let Some(index) = self.try_alloc_in_place(conn, len) {
+    fn alloc_segment<D>(
+        &mut self,
+        conn: &mut Connection<D>,
+        len: usize,
+        spec: BufferSpec,
+    ) -> usize {
+        if let Some(index) = self.try_alloc_in_place(conn, len, spec) {
             return index;
         }
-        self.merge_segments(conn);
-        if let Some(index) = self.try_alloc_in_place(conn, len) {
+
+        self.defragment(conn);
+        if let Some(index) = self.try_alloc_in_place(conn, len, spec) {
             return index;
         }
 
         match self.segments.last_mut() {
-            Some(segment) if segment.refcnt == 0 => {
+            Some(segment)
+                if segment
+                    .refcnt
+                    .compare_exchange(0, 1, Ordering::AcqRel, Ordering::Acquire)
+                    .is_ok() =>
+            {
                 if let Some(buffer) = segment.buffer.take() {
-                    buffer.wl.destroy(conn);
+                    buffer.0.destroy(conn);
                 }
                 let new_size = self.len + len - segment.len;
                 segment.len = len;
-                segment.refcnt = 1;
-                segment.id = alloc_id!(self);
                 self.resize(conn, new_size);
             }
             _ => {
                 let offset = self.len;
                 self.resize(conn, self.len + len);
                 self.segments.push(Segment {
-                    id: alloc_id!(self),
                     offset,
                     len,
-                    refcnt: 1,
+                    refcnt: Arc::new(AtomicU32::new(1)),
                     buffer: None,
                 });
             }
