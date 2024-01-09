b522992dcaf20c0332a597518eece074a0553bd4
MaxVerevkin <maxxverrr@gmail.com>
2024-01-10 00:34:38 +0200

    wayrs-proto-parser: v1.0.1

--- wayrs-proto-parser/src/types.rs
+++ wayrs-proto-parser/src/types.rs
@@ -1,13 +1,13 @@
 use std::borrow::Cow;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Protocol<'a> {
     pub name: String,
     pub description: Option<Description<'a>>,
     pub interfaces: Vec<Interface<'a>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Interface<'a> {
     pub name: String,
     pub version: u32,
@@ -17,7 +17,7 @@ pub struct Interface<'a> {
     pub enums: Vec<Enum<'a>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Message<'a> {
     pub name: String,
     pub kind: Option<String>,
@@ -26,7 +26,7 @@ pub struct Message<'a> {
     pub args: Vec<Argument>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Enum<'a> {
     pub name: String,
     pub is_bitfield: bool,
@@ -34,13 +34,13 @@ pub struct Enum<'a> {
     pub items: Vec<EnumItem>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Description<'a> {
     pub summary: Option<String>,
     pub text: Option<Cow<'a, str>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Argument {
     pub name: String,
     pub arg_type: String,
@@ -50,7 +50,7 @@ pub struct Argument {
     pub summary: Option<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct EnumItem {
     pub name: String,
     pub value: u32,
